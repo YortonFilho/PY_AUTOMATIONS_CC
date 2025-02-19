b581504a89d5ea4ee5d21c05a63b8bb816a3af7f
Yorton <155913876+YortonFilho@users.noreply.github.com>
2025-02-19 09:29:46 -0300

    Delete DRC_ENVIO_FARMACIAS/src directory

--- DRC_ENVIO_FARMACIAS/src/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-import modules.config
-import modules.email_functions
-import modules.logger
-import modules.oracle_database
-import modules.process_data
--- DRC_ENVIO_FARMACIAS/src/modules/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-# Logging configuration
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
-
-# Function to generate logs
-def get_logger():
-    return logging.getLogger()
